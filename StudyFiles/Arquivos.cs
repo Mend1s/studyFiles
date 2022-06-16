c5cc2aafce021c16f61925d22aae1657a0f465cc
Mend1s <andremendesskt99@gmail.com>
2022-06-16 00:43:51 -0300

    Add project files.

---
+++ StudyFiles/Arquivos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudyFiles
+{
+    public class Arquivos
+    {
+        public string diretorioPadrao { get; private set; }
+        public string diretorioDestino { get; private set; }
+        public Arquivos(string diretorioPadrao)
+        {
+
+        }
+
+        public Arquivos(string diretorioPadrao, string diretorioDestino)
+        {
+
+        }
+
+    }
+}
---
+++ StudyFiles/Program.cs
@@ -0,0 +1,5 @@
+﻿using System.IO;
+
+string diretorioPadrao = Console.ReadLine();
+Console.WriteLine("Hello, World!");
+Console.WriteLine(diretorioPadrao);
